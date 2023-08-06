819fb15103fa20e46481fec70a4b0fa2bc1db619
Edwin Keijl <e.keijl89@gmail.com>
2023-08-06 15:33:32 +0000

    Update SpeciesPage.tsx

--- src/pages/SpeciesPage.tsx
+++ src/pages/SpeciesPage.tsx
@@ -1,4 +1,5 @@
-import { useState, useMemo, useRef } from "react";
+import { useMemo, useRef } from "react";
+import { useLocalStorage, useElementSize } from "usehooks-ts";
 import { Toggle } from "../components/Toggle";
 import * as THREE from "three";
 import ForceGraph3D from "react-force-graph-3d";
@@ -7,8 +8,6 @@ import type {
   LinkObject,
   NodeObject,
 } from "react-force-graph-3d";
-
-import { useElementSize } from "usehooks-ts";
 
 import * as s from "../data/species";
 import { Page } from "../components/Page";
@@ -35,46 +34,11 @@ export interface Link extends Omit<LinkObject, "source"> {
   color: string;
 }
 
-const getNodesForSpecies = (species: Record<string, Species>) => {
-  const speciesNodes: SpeciesNode[] = [];
-  const speciesLinks: Link[] = [];
+const GROUP_SPECIES = 0;
+const GROUP_NEEDS = 1;
+const GROUP_SERVICES = 2;
 
-  const needsSet = new Set<string>();
-  const servicesSet = new Set<string>();
-
-  Object.entries(species).forEach(([species, { needs, services, color }]) => {
-    speciesNodes.push({ id: species, group: 0 });
-
-    needs.forEach((need) => {
-      needsSet.add(need);
-      speciesLinks.push({
-        source: species,
-        target: need,
-        group: 1,
-        color,
-      });
-    });
-    services.forEach((service) => {
-      servicesSet.add(service);
-      speciesLinks.push({
-        source: species,
-        target: service,
-        group: 2,
-        color,
-      });
-    });
-  });
-
-  needsSet.forEach((need) => {
-    speciesNodes.push({ id: need, name: need, group: 1 });
-  });
-
-  servicesSet.forEach((service) => {
-    speciesNodes.push({ id: service, name: service, group: 2 });
-  });
-
-  return { nodes: speciesNodes, links: speciesLinks };
-};
+const speciesData = Object.values({ ...s });
 
 const Species = ({ species: { id, color } }: { species: Species }) => (
   <img
@@ -85,35 +49,33 @@ const Species = ({ species: { id, color } }: { species: Species }) => (
   />
 );
 
+const speciesDefault: string[] = [];
+
 const SpeciesPage = (): JSX.Element => {
   const graphRef = useRef<ForceGraphMethods>();
-
   const [containerRef, { width, height }] = useElementSize();
 
-  const [showFood, setFood] = useState(true);
-  const [showServices, setServices] = useState(true);
-  const [speciesSet, updateSpecies] = useState(new Set(Object.keys(s)));
+  const [speciesIds, setSpecies] = useLocalStorage("species", speciesDefault);
+  const [showNeeds, setNeeds] = useLocalStorage("showNeeds", true);
+  const [showServices, setServices] = useLocalStorage("showServices", true);
+
+  const speciesSet = useMemo(() => new Set(speciesIds), [speciesIds]);
 
   const toggleSpecies = (type: string) => {
-    if (speciesSet.has(type)) {
-      speciesSet.delete(type);
-    } else {
-      speciesSet.add(type);
-    }
-
-    updateSpecies(new Set([...speciesSet]));
+    setSpecies((species) => {
+      if (species.includes(type)) {
+        return species.filter((s) => s !== type);
+      } else {
+        return Array.from(new Set([...species, type]));
+      }
+    });
   };
-
-  const data = useMemo(() => {
-    const species = { ...s };
-    return getNodesForSpecies(species);
-  }, []);
 
   const renderSpeciesObject = ({ id, group }: SpeciesNode) => {
     const imgTexture = new THREE.TextureLoader().load(
-      group === 0
+      group === GROUP_SPECIES
         ? `./img/species/${id}.png`
-        : group === 1
+        : group === GROUP_NEEDS
         ? `./img/goods/${id}.webp`
         : `./img/services/${id}.png`
     );
@@ -126,28 +88,58 @@ const SpeciesPage = (): JSX.Element => {
     return sprite;
   };
 
-  // Need to reset the whole graph in order to recalculate node/link visibility
-  const key = useMemo(
-    () => String([...speciesSet.values(), showFood, showServices]),
-    [speciesSet, showFood, showServices]
-  );
+  const data = useMemo(() => {
+    const nodes: SpeciesNode[] = [];
+    const links: Link[] = [];
 
-  const visibleNeeds = useMemo(() => {
-    const serviceSet = new Set<string>();
+    const needsSet = new Set<string>();
+    const servicesSet = new Set<string>();
 
-    speciesSet.forEach((species) => {
-      const speciesObject: { [_s: string]: Species } = { ...s };
-      speciesObject[species].services.forEach((s) => serviceSet.add(s));
-      speciesObject[species].needs.forEach((n) => serviceSet.add(n));
+    speciesData
+      .filter((s) => speciesSet.has(s.id))
+      .forEach(({ id, needs, services, color }) => {
+        nodes.push({ id, group: GROUP_SPECIES });
+
+        if (showNeeds) {
+          needs.forEach((need) => {
+            needsSet.add(need);
+            links.push({
+              source: id,
+              target: need,
+              group: GROUP_NEEDS,
+              color,
+            });
+          });
+        }
+
+        if (showServices) {
+          services.forEach((service) => {
+            servicesSet.add(service);
+            links.push({
+              source: id,
+              target: service,
+              group: GROUP_SERVICES,
+              color,
+            });
+          });
+        }
+      });
+
+    needsSet.forEach((need) => {
+      nodes.push({ id: need, name: need, group: GROUP_NEEDS });
     });
 
-    return serviceSet;
-  }, [speciesSet]);
+    servicesSet.forEach((service) => {
+      nodes.push({ id: service, name: service, group: GROUP_SERVICES });
+    });
+
+    return { nodes, links };
+  }, [speciesSet, showNeeds, showServices]);
 
   return (
     <Page className="species-page" isFullHeight>
       <div className="species-toggles">
-        {Object.values(s).map((species) => (
+        {speciesData.map((species) => (
           <label key={species.id}>
             <Toggle
               checked={speciesSet.has(species.id)}
@@ -158,7 +150,7 @@ const SpeciesPage = (): JSX.Element => {
         ))}
 
         <label>
-          <Toggle checked={showFood} onChange={() => setFood(!showFood)} />
+          <Toggle checked={showNeeds} onChange={() => setNeeds(!showNeeds)} />
           Needs
         </label>
 
@@ -172,48 +164,20 @@ const SpeciesPage = (): JSX.Element => {
       </div>
 
       <div className="species-graph" ref={containerRef}>
-        {data ? (
-          <ForceGraph3D
-            key={key}
-            graphData={data}
-            ref={graphRef}
-            width={width}
-            height={height}
-            backgroundColor="rgb(0,0,0,0.5)"
-            nodeVisibility={(node: SpeciesNode) => {
-              return (
-                (node.group === 0 && speciesSet.has(String(node.id))) ||
-                (node.group === 1 &&
-                  showFood &&
-                  visibleNeeds.has(String(node.id))) ||
-                (node.group === 2 &&
-                  showServices &&
-                  visibleNeeds.has(String(node.id)))
-              );
-            }}
-            nodeThreeObject={renderSpeciesObject}
-            linkColor={(link) => (link as Link).color}
-            linkCurvature={0.3}
-            linkOpacity={0.5}
-            linkVisibility={(link: any) => {
-              if (
-                !visibleNeeds.has(link.target.id) ||
-                !speciesSet.has(link.source.id)
-              )
-                return false;
-
-              return (
-                (link.group === 1 && showFood) ||
-                (link.group === 2 && showServices)
-              );
-            }}
-            numDimensions={3} // 3D graph
-            cooldownTicks={100}
-            onEngineStop={() => graphRef?.current?.zoomToFit(400, 1)}
-          />
-        ) : (
-          <p>No species!</p>
-        )}
+        <ForceGraph3D
+          graphData={data}
+          ref={graphRef}
+          width={width}
+          height={height}
+          backgroundColor="rgb(0,0,0,0.5)"
+          nodeThreeObject={renderSpeciesObject}
+          linkColor={(link) => (link as Link).color}
+          linkCurvature={0.3}
+          linkOpacity={0.5}
+          numDimensions={3} // 3D graph
+          cooldownTicks={100}
+          onEngineStop={() => graphRef?.current?.zoomToFit(400, 1)}
+        />
       </div>
     </Page>
   );
